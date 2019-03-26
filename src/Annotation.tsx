1afe3b3e20ed079ce1d45257c978d256c18117a8
nicolas mondon <nicolasmondon@gmail.com>
2019-03-26 10:17:07 +0100

    patch style of label container

--- src/Annotation.tsx
+++ src/Annotation.tsx
@@ -188,6 +188,7 @@ const Annotation = ({
                     <div
                         style={{
                             ...{
+                                width: '100%',
                                 overflowWrap: 'break-word',
                                 padding: '5px',
                                 boxSizing: 'border-box',
